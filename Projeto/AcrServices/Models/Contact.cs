7be1c2d4930069e9e0da6225cbfd93a2c28954ab
Túlio Rezende <tulio.mamute@hotmail.com>
2017-05-18 11:40:27 -0300

    Criaçãode layout de fale conosco

---
+++ Projeto/AcrServices/Models/Contact.cs
@@ -0,0 +1,27 @@
+﻿using System;
+using System.Collections.Generic;
+using System.ComponentModel;
+using System.Linq;
+using System.Web;
+
+namespace AcrServices.Models
+{
+    public class Contact
+    {
+        [DisplayName("Nome")]
+        public string Nome { get; set; }
+
+        [DisplayName("Telefone Para Contato")]
+        public string Telefone { get; set; }
+
+        [DisplayName("Email")]
+        public string Email { get; set; }
+
+        [DisplayName("Assunto")]
+        public string Assunto { get; set; }
+
+        [DisplayName("Corpo Email")]
+        public string CorpoEmail { get; set; }
+    }
+
+}
